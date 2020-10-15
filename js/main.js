03480fb526c8810eb4da9ac6365b5757df9d7d5c
Massimiliano Ambrogio <maxi.ambrogio@gmail.com>
2020-10-15 21:56:23 +0200

    Project 15 final

--- js/main.js
+++ js/main.js
@@ -3,57 +3,31 @@
  */
 
  // Domande al cliente 
-var numkm = prompt('Quanti chilometri vuoi percorrere?');
+var distance = parseInt(prompt('Quanti chilometri vuoi percorrere?') );
 
-var passengerAge = prompt('Quanti anni hai?'); 
+var age = parseInt(prompt('Quanti anni hai?') ); 
 
 // Calcolo prezzo biglietto treno
-var kmprice = 0.21;
+var price = 0.21 * distance;
 
-var prezzoKm = numkm * kmprice;
+var message;
 
-console.log('numero km: ', numkm);
-
-console.log('Anni passeggero: ', passengerAge);
-
-console.log('Prezzo viaggio intero: ', prezzoKm.toFixed(2) + '€');
-
-document.getElementById('Totalprice').innerHTML = prezzoKm + '€';
-
-// Sconto per minorenni
-if (passengerAge < 18) {
-    console.log('Sconto biglietto del 20% per minorenne');
-
-    var scontoViaggio = (prezzoKm / 100 * 20);
-   
-    console.log('Prezzo viaggio minorenne: ', prezzoViaggio.toFixed(2) + '€');
-
-    var prezzoViaggio = (prezzoKm - scontoViaggio);
-
-    console.log('Sconto viaggio minorenne: ', scontoViaggio.toFixed(2) + '€');
+// Sconto minorenni
+if (age < 18) {
+    price -= price * 0.2;
+    message = 'Hai diritto allo sconto Minorenni del 20%!<br>Il prezzo è: ' + price.toFixed(2)
+        + '€';
 }
 
-else {
-    var prezzoViaggio = (prezzoKm).toFixed(2);
-}
-
-// Sconto per over 65
-if (passengerAge > 65) {
-    console.log('Sconto biglietto del 40% per over 65');
-
-    var scontoViaggio = (prezzoKm / 100 * 40);
-     
-    console.log('Prezzo viaggio over 65: ', prezzoViaggio.toFixed(2) + '€');
-
-    var prezzoViaggio = (prezzoKm - scontoViaggio);
-
-    console.log('Sconto viaggio over 65: ', scontoViaggio.toFixed(2) + '€');
-    
-    document.getElementById('Totalprice1').innerHTML = prezzoKm + '€'; 
+// Sconto over 65
+else if (age > 65) {
+    price -= price * 0.2;
+    message = 'Hai diritto allo sconto Senior del 40%!<br>Il prezzo è: ' + price.toFixed(2)
+        + '€';
 }
 
  else {
-    console.log('Sei maggiorenne ma non over 65, non ricevi lo sconto sul bigletto');
-    
-    var prezzoViaggio = (prezzoKm).toFixed(2);
- }
+    message = 'Il prezzo è: ' + price.toFixed(2) + '€';
+}
+ 
+document.getElementById('total').innerHTML = message; 
